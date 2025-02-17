221e8bc65730f812b9965dcc5fa7ab1e562bc8f5
Joerg Seitter <jseitter@users.noreply.github.com>
2025-02-17 12:39:27 +0100

    added some comments

--- backend/src/main/java/io/webetl/model/Project.java
+++ backend/src/main/java/io/webetl/model/Project.java
@@ -4,37 +4,51 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+/**
+ * Project is a collection of sheets.
+ * 
+ */
 public class Project {
+    // Project id
     private String id;
+    // Name of the project
     private String name;
+    // Sheets of the project
     private List<Sheet> sheets;
 
+    // Constructor
     public Project() {
         super();
         this.id = UUID.randomUUID().toString();
         this.sheets = new ArrayList<>();
     }
 
+    // Getter for id
     public String getId() {
         return id;
     }
 
+    // Setter for id
     public void setId(String id) {
         this.id = id;
     }
 
+    // Getter for name
     public String getName() {
         return name;
     }
 
+    // Setter for name
     public void setName(String name) {
         this.name = name;
     }
 
+    // Getter for sheets
     public List<Sheet> getSheets() {
         return sheets;
     }
 
+    // Setter for sheets
     public void setSheets(List<Sheet> sheets) {
         this.sheets = sheets;
     }
--- backend/src/main/java/io/webetl/model/Sheet.java
+++ backend/src/main/java/io/webetl/model/Sheet.java
@@ -2,12 +2,23 @@ package io.webetl.model;
 
 import java.util.List;
 import java.util.Map;
-
+/**
+ * Sheet is the main object in the WebETL platform.
+ * It represents a flow of data from a source to a destination.
+ * It contains the nodes and edges of the flow.
+ * 
+ * 
+ */
 public class Sheet {
+    // Sheet id
     private String id;
+    // Version of the sheet
     private Integer version;
+    // Name of the sheet
     private String name;
+    // Nodes of the sheet
     private List<Map<String, Object>> nodes;
+    // Edges of the sheet
     private List<Map<String, Object>> edges;
 
     // Getters and setters
