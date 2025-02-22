f4468948b41549ae9d20be6ecb3015b1261c7aad
Joerg Seitter <jseitter@users.noreply.github.com>
2025-02-22 10:31:00 +0100

    added new component to test the dynamic palette feature

---
+++ backend/src/main/java/io/webetl/components/MapTransformComponent.java
@@ -0,0 +1,31 @@
+package io.webetl.components;
+
+import io.webetl.annotation.ETLComponentDefinition;
+import io.webetl.model.component.TransformComponent;
+import io.webetl.model.component.parameter.SQLParameter;
+import io.webetl.runtime.ExecutionContext;
+
+@ETLComponentDefinition(
+    id = "map-transform",
+    label = "Map Transform",
+    description = "Maps input data to a new schema using SQL expressions",
+    icon = "MapIcon",
+    backgroundColor = "#fff3e0"
+)
+public class MapTransformComponent extends TransformComponent {
+    
+    public MapTransformComponent() {
+        getParameters().add(SQLParameter.builder()
+            .name("mappingExpression")
+            .label("Mapping Expression")
+            .description("SQL expression to map input fields to output fields (e.g., SELECT field1 as newField1, field2 as newField2)")
+            .required(true)
+            .build());
+    }
+
+    @Override
+    public void execute(ExecutionContext context) {
+        // Implementation for mapping transformation
+        // Will be implemented in a future PR
+    }
+} 
